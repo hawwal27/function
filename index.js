0d26a0fd5fd16f95cef8e3bb4d7b92db6851f258
ijaolahawwalolamilekan <ijaolahawwal@gmaol.com>
2024-11-01 06:27:32 -0800

    newly added

---
+++ index.js
@@ -0,0 +1,175 @@
+// function greeting (){
+//     console.log("awwal welcome to javascript")
+// }
+
+// greeting()
+// greeting()
+// for (let i=1; i<=100; i++){
+//     greeting()
+// }
+// const greeting=()=>{
+//     console.log("awwal welcome to javascript")
+// }
+// greeting()
+// greeting()
+// for (let i=1; i<=100; i++){
+//     greeting()
+// }
+// function greeting (name){
+//     console.log(`${name} welcome to javascript`);
+// }
+// greeting ("awwal")
+// greeting ("usama")
+// for (let i=1; i<=100; i++){
+//     greeting("anybody")
+// }
+// const greeting=(name)=>{
+//     console.log(`${name} welcome to javascript`)
+// }
+// greeting ("awwal")
+// greeting ("usama")
+// for (let i=1; i<=100; i++){
+//     greeting("anybody")
+// }
+// let name = prompt("enter your name")
+// let age = prompt ("enter your age")
+// function greeting (name,age){
+//     alert(`hello ${name} welcome to the world of robotics, you are ${age}`)
+//     console.log(`${name} welcome to javascript`)
+// }
+// greeting (`${name}`)
+// greeting (`${name}`)
+// for (let i=1; i<=100; i++){
+//     greeting (`${name}`)
+// }
+//initializing argument 
+// function add (a=1, b=1){
+//     let C;
+//     C = a+b;
+//     return C;
+// }
+// console.log(add());
+/*
+comsole.log (add (10));
+console.log (add (10,2)); */
+// function greeting (name =("name"), _age=18){
+//     alert(`Hello ${name} your age is ${18}`)
+// }
+// greeting ("Hadii", 10);
+// console.log (add (10,20))
+// let result = add (10,20)
+// console.log (result);
+
+
+
+
+//Returning more than one variable in a function 
+
+// function address (name = "dummy", age = 16){
+//     console.log (`the program return ${name} at ${age} address`)
+//     return ["street number 101, a street ajanlekoko" ]
+// }
+// let number, streetname = address ("Hadith",8);
+// console.log ( number, streetname)
+
+//Function in a function 
+
+// function greeting (){
+//     function message () {
+//         console.log ("Hello world ");
+//     }
+//     message ()
+// }
+// let result = add (10,20)
+// greeting ()
+
+//Passing function as an argument
+
+
+// function methods (function (){ console.log ("you have been gifted 100gb")},Rate){
+//     console.log(subData (), rate );
+// }
+// methods (sub-data , 100 gb/s)
+
+
+//Returning a function 
+
+// function maths (){
+//     return function (a,b){
+//         let c = a-b;
+//         console.log (`a - b = ${c}`)
+//     }
+// }
+// let subtraction = maths()
+// subtraction (100,20)
+
+// const greeting = () => {
+//     return ("Hello world")
+// }
+// greeting ()
+
+// const greeting = () =>("Hello world")
+// console.log(greeting())
+// function subscription ( (data) => {console.log(`Your gifted data is ${data}`)},rates){
+//     subData (250 GB);
+//     return rate ;
+// }
+// let rates = subscription (subData, 200gb/s)
+// console.log(rates)
+
+
+
+
+
+// function circle (circumfrenceofcircle){
+//     console.log (3.14159*34)
+// }
+// circle()
+
+
+
+// function area (areaOfcone){
+//     console.log( 3.14159*5*(4+5))
+// }
+// area()
+
+
+
+// function volume(volumeofcuboid){
+// console.log(5*4*7)
+// }
+// volume()
+
+
+// function volume(volumeofcylinder){
+//     console.log(3.14159*3**2*6)
+// }
+// volume()
+
+
+// function perimeter(perimeterofrectangle){
+//     console.log(2*(4+6))
+// }
+// perimeter()
+
+
+
+// function volume(volumeofsphere){
+//     console.log(4/3*3.14159*4**2)
+// }
+// volume()
+
+
+
+// function perimeter (perimeterofsquare){
+//     console.log(4+4+4+4)
+// }
+// perimeter()
+
+
+
+
+// function area (areaOftriangle){
+//     console.log(7*5)/2
+// }
+// area()
